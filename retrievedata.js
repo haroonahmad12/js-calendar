452fdba6618650fabf1e489a6d6422500e680232
Haroon Ahmad <haroon334@gmail.com>
2021-07-23 09:59:18 +0200

    updating functions

---
+++ retrievedata.js
@@ -0,0 +1,24 @@
+/*var nextMonthEvents = document.getElementById("next-month");
+var prevMonthEvents = document.getElementById("prev-month");
+
+// --------------ADD EVENT LISTENERS
+
+nextMonthEvents.addEventListener("click", printNextEvents);
+prevMonthEvents.addEventListener("click", printPrevEvents);
+
+function printNextEvents() {
+    var dias = document.querySelectorAll(".every_date");
+
+    for (let i = 0; i < 42; i++) {
+        let allDaysId = dias[i].getAttribute("id");
+
+        allDaysId = allDaysId.split(" ");
+
+        console.log(typeof allDaysId);
+    }
+}
+
+function printPrevEvents() {
+    console.log("read correct");
+}
+*/
